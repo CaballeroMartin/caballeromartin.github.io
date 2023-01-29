ab34d9ebff6d29e184413c666beab6c731d2ccbc
CaballeroMartin <85260278+CaballeroMartin@users.noreply.github.com>
2023-01-29 18:23:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function onClickOne() {
+  document.getElementById("info1").innerHTML =
+    "Técnico Operador de Sonido y Grabación con especialización en Producción Profesional de Música";
+}
+
+function onClickTwo() {
+  document.getElementById("info2").innerHTML = "First Certificate in English";
+  document.getElementById("info3").innerHTML = "Preliminary English Test";
+}
